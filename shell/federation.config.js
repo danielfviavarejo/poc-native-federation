6b26d8e28cd32c91c455abffd07d9f2045f66ad1
daniel <deecomunicacao@gmail.com>
2024-10-16 10:36:52 -0300

    feat: add libs

--- shell/federation.config.js
+++ shell/federation.config.js
@@ -1,20 +1,24 @@
 const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
 
 module.exports = withNativeFederation({
-
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
   },
 
   skip: [
-    'rxjs/ajax',
-    'rxjs/fetch',
-    'rxjs/testing',
-    'rxjs/webSocket',
+    "rxjs/ajax",
+    "rxjs/fetch",
+    "rxjs/testing",
+    "rxjs/webSocket",
+    "@vv/viamais-app-android-communication",
+    "@vv/viamais-app-utils",
     // Add further packages you don't need at runtime
-  ]
+  ],
 
   // Please read our FAQ about sharing libs:
   // https://shorturl.at/jmzH0
-  
 });
